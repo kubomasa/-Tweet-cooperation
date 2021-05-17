ef69e9191d4420314f8ee49420ab67db00562ce6
kubomasa <83449843+kubomasa@users.noreply.github.com>
2021-05-17 23:51:43 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-document.querySelector('#tweetButton').addEventListener('click',()=>{
-  //ツイート内容を取得
-  let tweetText = document.querySelector('#tweetTextArea').Value;
+document.querySelector('#tweetButton').addEventListener('click', () => {
+  // ツイート内容を取得
+  let tweetText = document.querySelector('#tweetTextArea').value;
 
-  //半角スペースと＃JavaScriptをツイート文言に追加する
-  tweetText +=' #JavaScript';
+  // 半角スペースと #JavaScriptをツイート文言に追加する
+  tweetText += ' #自動でタグが付く';
 
-  //エンコードする
+  // エンコードする
   const encodedText = encodeURIComponent(tweetText);
 
-  //ツイート用リンクを作成する
-  const tweetURL=`https://twitter.com/compose/tweet?text=${encodedText}`;
+  // ツイート用リンクを作成する
+  const tweetURL =
+    `https://twitter.com/intent/tweet?text=${encodedText}`;
 
-  //ツイート用リンクを開く
+  // ツイート用リンクを開く
   window.open(tweetURL);
 });
